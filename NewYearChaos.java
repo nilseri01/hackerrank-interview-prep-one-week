0c60bee78f33213e32280f526e86a529aa80ee5c
Nil Seri <senoritadeveloper01@gmail.com>
2022-06-14 21:53:49 +0300

    Create NewYearChaos.java

---
+++ NewYearChaos.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+// https://www.hackerrank.com/challenges/one-week-preparation-kit-new-year-chaos/problem
+
+class Result {
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<Integer> q) {
+    // Write your code here
+        int bribesCount = 0;
+        for (int i = q.size() - 1; i >= 0; i--) {
+            if (q.get(i) - 2 > i + 1) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            int j = Math.max(0, q.get(i) - 2);
+            for (; j < i; j++) {
+                if (q.get(j) > q.get(i)) {
+                    bribesCount++;
+                }
+            }
+        }
+        System.out.println(bribesCount);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
